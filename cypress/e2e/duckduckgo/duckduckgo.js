b563d83fb719f3c04d8edd9cd4d90ceebd0ed92f
PedroFernandes77 <pedromrfernandes@hotmail.com>
2023-04-11 18:48:53 +0100

    second

---
+++ cypress/e2e/duckduckgo/duckduckgo.js
@@ -0,0 +1,7 @@
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor/steps';
+When('I visit duckduckgo.com', () => {
+    //TODO:
+});
+Then('I should see a search bar', () => {
+    //TODO:
+});
